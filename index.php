b0aa04f8876d7c2f329048bc299476545541e972
Mauricio Fuentes <mauriciofb@gmail.com>
2024-01-29 15:00:10 -0300

    Save and display greeting messages

--- index.php
+++ index.php
@@ -50,13 +50,36 @@ $messageform = new \local_greetings\form\message_form();
 
 $messageform->display();
 
+// Message display.
+$messages = $DB->get_records('local_greetings_messages');
+
+echo $OUTPUT->box_start('card-columns');
+
+foreach ($messages as $m) {
+    echo html_writer::start_tag('div', array('class' => 'card-body'));
+    echo html_writer::start_tag('div', array('class' => 'card'));
+    echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::start_tag('p', array('class' => 'card-text'));
+    echo html_writer::tag('small', userdate($m->createdtime), array('class' => 'text-muted'));
+    echo html_writer::end_tag('p');
+    echo html_writer::end_tag('div');
+    echo html_writer::end_tag('div');
+}
+
+echo $OUTPUT->box_end();
+
+
+// Form retrive info and saving into DB.
 if ($data = $messageform->get_data()) {
     $message = required_param('message', PARAM_TEXT);
 
-    echo $OUTPUT->heading($message, 4);
-}
+    if (!empty($message)) {
+        $record = new stdClass;
+        $record->message = $message;
+        $record->createdtime = time();
 
-echo html_writer::tag('h4', get_string('thecurrenttimeis', 'local_greetings'));
-echo userdate(time(), get_string('strftimedaydate', 'core_langconfig'));
+        $DB->insert_record('local_greetings_messages', $record);
+    }
+}
 
 echo $OUTPUT->footer();
